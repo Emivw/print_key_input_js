4803cde0ef689f55a2951d7b8ae708ef72edaf13
Emivw <emiya.vanwyk@gmail.com>
2022-07-05 11:46:43 +0200

    first commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+    data: function() {
+        return {
+            show: false,
+        };
+    },
+    methods: {
+        handleKeyPressed(e) {
+            console.log(e);
+            console.log(e.key);
+            if ((e.code = "KeyA")) {
+                this.show = true;
+            }
+            if (e.code != "KeyA") {
+                this.show = false;
+            }
+        },
+    },
+});
+app.mount("#app");
